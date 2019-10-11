d824b9e040bcd03fe1f7d8ad813513b20cf0bd81
python283 <56442737+python283@users.noreply.github.com>
2019-10-12 01:18:06 +0900

    time and websoket

---
+++ clock.js
@@ -0,0 +1,40 @@
+// 時計のメインとなる関数
+function clock()
+{
+    // 曜日を表す各文字列の配列
+    var weeks = new Array("Sun","Mon","Thu","Wed","Thr","Fri","Sat");
+    // 現在日時を表すインスタンスを取得
+    var now = new Date();
+    // 年
+    var y = now.getFullYear();
+    // 月 0~11で取得されるので実際の月は+1したものとなる
+    var mo = now.getMonth() + 1;
+    // 日
+    var d = now.getDate();
+    // 曜日 0~6で日曜始まりで取得されるのでweeks配列のインデックスとして指定する
+    var w = weeks[now.getDay()];
+    // 時
+    var h = now.getHours();
+    // 分
+    var mi = now.getMinutes();
+    // 秒
+    var s = now.getSeconds();
+
+    // 日付時刻文字列のなかで常に2ケタにしておきたい部分はここで処理
+    if (mo < 10) mo = "0" + mo;
+    if (d < 10) d = "0" + d;
+    if (mi < 10) mi = "0" + mi;
+    if (s < 10) s = "0" + s;
+
+    //　HTML: <span id="clock_date">(ココの日付文字列を書き換え)</span>
+    document.getElementById("clock_date").innerHTML =  y + "/" + mo + "/" + d + " (" + w + ")";
+    //　HTML: <span id="clock_time">(ココの時刻文字列を書き換え)</span>
+    document.getElementById("clock_time").innerHTML = h + ":" + mi + ":" + s;
+    //　HTML: <div id="clock_frame"> の内部要素のフォントサイズをウインドウサイズの10分の1ピクセルに設定
+    document.getElementById("clock_frame").style.fontSize =  window.innerWidth / 20 + "px";
+  
+    
+}
+
+// 上記のclock関数を1000ミリ秒ごと(毎秒)に実行する
+setInterval(clock, 1000);
---
+++ webs.js
@@ -0,0 +1,27 @@
+
+const channelName = "lightning_executions_FX_BTC_JPY";
+
+// note: reconnection handling needed.
+const ws = new WebSocket("wss://ws.lightstream.bitflyer.com/json-rpc");
+
+ws.addEventListener("open", () => {
+ws.send(JSON.stringify({
+    method: "subscribe",
+    params: {
+    channel: channelName
+    },
+    id: 123
+}));
+});
+
+ws.addEventListener("message", msgEvent => {
+const data = JSON.parse(msgEvent.data);
+
+if (data.id === 123) {
+    console.log("subscribed!");
+}
+if (data.params && data.params.channel && data.params.message) {
+    console.log(data.params.channel, data.params.message);
+    document.getElementById("rest_api").innerHTML = data.params.message[0].price + '<br>';
+}
+});
